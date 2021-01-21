4a8283758fcfb17064f0f3e43a784080458a7465
Michelle Smith <smith.michelle.a@gmail.com>
2021-01-21 15:17:19 -0500

    added variable time blocks

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+// time blocks
+var timeBlocks = [
+    {
+        hour = 9,
+        events = "",
+    }
+]
+
+// go through the time bloacks, for loop or for each.
+
+// Display current data in header
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
