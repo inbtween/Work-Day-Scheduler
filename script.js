0ab45fc236368ff54b83d32bd1ff0e31ac745a8f
inbtween <76419703+inbtween@users.noreply.github.com>
2021-01-21 19:41:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// time blocks
+var timeBlocks = [
+    { time = 9,
+    events = "",
+    }
+]
+
+// go through the time bloacks, for loop or for each.
+
+// Display current data in header
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
