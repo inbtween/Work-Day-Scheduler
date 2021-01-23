bb293893c856712dc161a3ad4419e699e5a43f3a
Michelle Smith <smith.michelle.a@gmail.com>
2021-01-22 21:20:18 -0500

    added for loop for time + row and save btn for timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -6,16 +6,27 @@ var schedule = ["", "", "", "", "", "", "", "", ""];
 var timeWindow = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 /// THEN each time block is color-coded to indicate whether it is in the past, present, or future
 var timeFuture = "future";
+var timeBlock = $(".container");
 // FUNCTIONS-----------------
 
 // save button will store appt in local storage
 // add event listener
-$("button").on("click"),
-  function () {
-    localStorage.setItem("timeBlocks", JSON.stringify(timeBlocks));
-    var getEvent = JSON.parse(localStorage);
-  };
+$("button").on("click", function () {
+  var schedule = $(".description").val();
+  console.log(schedule);
+  localStorage.setItem("", JSON.stringify(timeBlocks));
+  var getEvent = JSON.parse(localStorage);
+});
 
+for (var i = 0; 0 < 9; i++) {
+  var divTag = $('<div class= "row time-block">');
+  divTag.html(`<class="hour col-md-1">${timeWindow[i]}:00</div>
+  <textarea class="description col-md-10 ${timeFuture}" id="saveBtn-${i}"></textarea>
+  <button class="col-md-1 btn saveBtn" value="${i}">Save</button>`);
+  timeBlock.append(divTag);
+}
+
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 // USER INTERACTIONS--------------
 // WHEN I click into a time block
 // THEN I can enter an event
@@ -67,4 +78,3 @@ $("button").on("click"),
 // go through the time blocks, for loop or for each.
 
 // Display current data in header
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
